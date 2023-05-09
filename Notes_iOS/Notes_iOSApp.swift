d3784ac02ea22370da40bacac7f0387d2e5cfc52
Junaed <junaed1995@gmail.com>
2023-05-09 11:55:41 +0600

    Initial Commit

---
+++ Notes_iOS/Notes_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_iOSApp.swift
+//  Notes_iOS
+//
+//  Created by Junaed Muhammad Chowdhury on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Notes_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
